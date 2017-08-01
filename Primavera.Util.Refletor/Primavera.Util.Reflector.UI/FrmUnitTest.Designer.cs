730bf288fa7b13c41d996d2e09878b5797332b2c
RumpleZ <dwp4601@hotmail.com>
2017-08-01 14:15:17 +0100

    Add new utilitário

---
+++ Primavera.Util.Refletor/Primavera.Util.Reflector.UI/FrmUnitTest.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Primavera.Util.Reflector.UI
+{
+    partial class FrmUnitTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btProcess = new System.Windows.Forms.Button();
+            this.TxtDestiny = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btGetFilePath = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilePath.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtFilePath.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.txtFilePath.Location = new System.Drawing.Point(110, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(269, 20);
+            this.txtFilePath.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Source";
+            // 
+            // btProcess
+            // 
+            this.btProcess.BackColor = System.Drawing.Color.Transparent;
+            this.btProcess.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btProcess.Location = new System.Drawing.Point(318, 67);
+            this.btProcess.Name = "btProcess";
+            this.btProcess.Size = new System.Drawing.Size(109, 40);
+            this.btProcess.TabIndex = 7;
+            this.btProcess.Text = "Create Test Files";
+            this.btProcess.UseVisualStyleBackColor = false;
+            this.btProcess.Click += new System.EventHandler(this.btProcess_Click);
+            // 
+            // TxtDestiny
+            // 
+            this.TxtDestiny.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtDestiny.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.TxtDestiny.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.TxtDestiny.Location = new System.Drawing.Point(113, 41);
+            this.TxtDestiny.Name = "TxtDestiny";
+            this.TxtDestiny.Size = new System.Drawing.Size(266, 20);
+            this.TxtDestiny.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Destiny";
+            // 
+            // btGetFilePath
+            // 
+            this.btGetFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btGetFilePath.Location = new System.Drawing.Point(385, 12);
+            this.btGetFilePath.Name = "btGetFilePath";
+            this.btGetFilePath.Size = new System.Drawing.Size(29, 23);
+            this.btGetFilePath.TabIndex = 10;
+            this.btGetFilePath.Text = "...";
+            this.btGetFilePath.UseVisualStyleBackColor = true;
+            this.btGetFilePath.Click += new System.EventHandler(this.btGetFilePath_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(385, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 23);
+            this.button1.TabIndex = 11;
+
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmUnitTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 119);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btGetFilePath);
+            this.Controls.Add(this.TxtDestiny);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btProcess);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label2);
+            this.Name = "FrmUnitTest";
+            this.Text = "FrmUnitTest";
+            //this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btProcess;
+        private System.Windows.Forms.TextBox TxtDestiny;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btGetFilePath;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Primavera.Util.Refletor/Primavera.Util.Reflector.UI/FrmUnitTest.cs
@@ -0,0 +1,127 @@
+﻿using Primavera.Util.Injector.Helpers;
+using Primavera.Util.Refletor.Entities;
+using Primavera.Util.Refletor.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Primavera.Util.Reflector.UI
+{
+    public partial class FrmUnitTest : Form
+    {
+
+        public FrmUnitTest()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Decompile decompile = new Decompile();
+                string inputPath = txtFilePath.Text.ToString();
+                string outputPath = TxtDestiny.Text.ToString();
+                System.Diagnostics.Debug.Print(inputPath);
+                // Examples:
+                // inputPath = "C:\\Users\\Primavera\\Desktop\\test folder\\BasBS100.dll";
+                // outputPath = "C:\\Users\\Primavera\\Desktop\\test folder1";
+
+                var result = inputPath.Substring(inputPath.LastIndexOf('\\') + 1);
+                string bs_name = result.Substring(0, 5);
+                System.Diagnostics.Debug.Print(bs_name);
+
+
+                if (File.Exists(inputPath))
+                {
+                    // This path is a file
+                    if (!Directory.Exists(inputPath))
+                        System.IO.Directory.CreateDirectory(outputPath);
+                }
+                else if (Directory.Exists(inputPath))
+                {
+                    // This path is a directory
+                    throw new NotImplementedException();
+                }
+                else
+                {
+                    System.Diagnostics.Debug.Print("{0} is not a valid file or directory.", (txtFilePath.ToString()));
+
+                    throw new Exception();
+                }
+                List<string> used_methods = new List<string>();
+
+                ModuleEntity moduleEntity = decompile.DecompileAssembly(inputPath);
+
+                foreach (TypeEntity typeEntity in moduleEntity.Types)
+                {
+                    System.Diagnostics.Debug.Print(typeEntity.Name);
+                    if (!typeEntity.Name.Substring(0, 5).Equals(bs_name))
+                        continue;
+                    string entityTypeName = typeEntity.Name.Substring(5);
+
+                    if (TypeHelper.HasPublicMethods(typeEntity) && entityTypeName.Length > 0)
+                    {
+                        if (!File.Exists(outputPath))
+                        {
+                            TextWriter tw = new StreamWriter(outputPath + "\\" + entityTypeName + ".cs");
+
+                            tw.WriteLine("using System;");
+                            tw.WriteLine("using Microsoft.VisualStudio.TestTools.UnitTesting;");
+                            tw.WriteLine("using StdBE100;");
+                            tw.WriteLine("using Primavera.Framework.Tests;");
+                            tw.WriteLine("");
+                            tw.WriteLine("[TestClass]");
+                            tw.WriteLine(string.Format("public class {0}Test : BaseTest", entityTypeName));
+                            tw.WriteLine("{");
+
+                            foreach (MethodEntity methodEntity in typeEntity.Methods)
+                            {
+                                if (!methodEntity.IsConstructor && methodEntity.IsPublic && !used_methods.Contains(methodEntity.Name))
+                                {
+                                    tw.WriteLine("    [TestMethod]");
+                                    tw.WriteLine("    [DataSource(\"System.Data.Odbc\", \"Dsn=Excel Files;Driver={Microsoft Excel Driver (*.xls)};dbq=|DataDirectory|\\\\DataFiles\\\\" + methodEntity.Name + ".xls;defaultdir=.\\\\DataFiles;\", \"Sheet1$\", DataAccessMethod.Sequential)]");
+                                    tw.WriteLine("    public void {0}_{1}()", entityTypeName, methodEntity.Name);
+                                    tw.WriteLine("    {");
+                                    tw.WriteLine("        // Insert your code here!");
+                                    tw.WriteLine("    " + "    // MotorLE.AntesActualizar" + MethodHelper.MethodSignature(methodEntity) + ";");
+                                    tw.WriteLine("    }" + Environment.NewLine);
+                                    used_methods.Add(methodEntity.Name);
+                                }
+                            }
+                            used_methods.Clear();
+                            tw.WriteLine("}");
+                            tw.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.FileTrace(ex.ToString());
+            }
+        }
+
+
+
+        private void btGetFilePath_Click(object sender, EventArgs e)
+        {
+            txtFilePath.Text = FileHelper.GetFile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TxtDestiny.Text = FileHelper.GetPath();
+
+        }
+    }
+}
