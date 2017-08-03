96180682359aecf1a38f89bb4dfc11519bdd272c
RumpleZ <dwp4601@hotmail.com>
2017-08-03 10:36:48 +0100

    utilitario modificado

--- Primavera.Util.Refletor/Primavera.Util.Reflector.UI/FrmUnitTest.cs
+++ Primavera.Util.Refletor/Primavera.Util.Reflector.UI/FrmUnitTest.cs
@@ -33,8 +33,8 @@ namespace Primavera.Util.Reflector.UI
                 string outputPath = TxtDestiny.Text.ToString();
                 System.Diagnostics.Debug.Print(inputPath);
                 // Examples:
-                // inputPath = "C:\\Users\\Primavera\\Desktop\\test folder\\BasBS100.dll";
-                // outputPath = "C:\\Users\\Primavera\\Desktop\\test folder1";
+                inputPath = @"C:\prjNET\ERP10\ERP\Mainline\_Bin\VndBS100.dll";
+                outputPath = "C:\\Users\\Primavera\\Desktop\\test2";
 
                 var result = inputPath.Substring(inputPath.LastIndexOf('\\') + 1);
                 string bs_name = result.Substring(0, 5);
@@ -58,13 +58,17 @@ namespace Primavera.Util.Reflector.UI
 
                     throw new Exception();
                 }
+
                 List<string> used_methods = new List<string>();
+                List<string> actual_params = new List<string>();
+                Dictionary<string, string> dictionary = new Dictionary<string, string>();
+                dictionary.Add("BasBS", "Base");
+                dictionary.Add("VndBS", "Vendas");
 
                 ModuleEntity moduleEntity = decompile.DecompileAssembly(inputPath);
 
                 foreach (TypeEntity typeEntity in moduleEntity.Types)
                 {
-                    System.Diagnostics.Debug.Print(typeEntity.Name);
                     if (!typeEntity.Name.Substring(0, 5).Equals(bs_name))
                         continue;
                     string entityTypeName = typeEntity.Name.Substring(5);
@@ -73,7 +77,7 @@ namespace Primavera.Util.Reflector.UI
                     {
                         if (!File.Exists(outputPath))
                         {
-                            TextWriter tw = new StreamWriter(outputPath + "\\" + entityTypeName + ".cs");
+                            TextWriter tw = new StreamWriter(outputPath + "\\" + entityTypeName + "Test.cs");
 
                             tw.WriteLine("using System;");
                             tw.WriteLine("using Microsoft.VisualStudio.TestTools.UnitTesting;");
@@ -89,11 +93,32 @@ namespace Primavera.Util.Reflector.UI
                                 if (!methodEntity.IsConstructor && methodEntity.IsPublic && !used_methods.Contains(methodEntity.Name))
                                 {
                                     tw.WriteLine("    [TestMethod]");
-                                    tw.WriteLine("    [DataSource(\"System.Data.Odbc\", \"Dsn=Excel Files;Driver={Microsoft Excel Driver (*.xls)};dbq=|DataDirectory|\\\\DataFiles\\\\" + methodEntity.Name + ".xls;defaultdir=.\\\\DataFiles;\", \"Sheet1$\", DataAccessMethod.Sequential)]");
+                                    tw.WriteLine("    [DataSource(\"System.Data.Odbc\", \"Dsn=Excel Files;Driver={Microsoft Excel Driver (*.xls)};dbq=|DataDirectory|\\\\DataFiles\\\\" + entityTypeName + "_" + methodEntity.Name + ".xls;defaultdir=.\\\\DataFiles;\", \"Sheet1$\", DataAccessMethod.Sequential)]");
                                     tw.WriteLine("    public void {0}_{1}()", entityTypeName, methodEntity.Name);
                                     tw.WriteLine("    {");
-                                    tw.WriteLine("        // Insert your code here!");
-                                    tw.WriteLine("    " + "    // MotorLE.AntesActualizar" + MethodHelper.MethodSignature(methodEntity) + ";");
+
+                                    ///////////////////
+
+                                    string call = "(";
+
+                                    foreach (MethodParameter atr in methodEntity.Parameters)
+                                    {
+                                        System.Diagnostics.Debug.Print(processa_tipos(atr));
+                                        tw.WriteLine(processa_tipos(atr));
+                                        call += (atr.ParameterType.IsByReference ? "ref " : string.Empty) + atr.Name + ",";
+                                    }
+                                    
+                                    var index = call.LastIndexOf(',');
+                                    if (index >= 0) call = call.Substring(0, index);
+
+                                    call += ");\n";
+
+                                    System.Diagnostics.Debug.Print(string.Format("        MotorLE.{0}.{1}.{2}{3}", dictionary[bs_name], entityTypeName, methodEntity.Name, call));
+                                    tw.WriteLine(string.Format("\n        MotorLE.{0}.{1}.{2}{3}", dictionary[bs_name], entityTypeName, methodEntity.Name, call));
+                                    tw.WriteLine("        // Insert your assert here!");
+
+                                    ///////////////////
+                                   
                                     tw.WriteLine("    }" + Environment.NewLine);
                                     used_methods.Add(methodEntity.Name);
                                 }
@@ -111,6 +136,51 @@ namespace Primavera.Util.Reflector.UI
             }
         }
 
+        private string processa_tipos(MethodParameter atr)
+        {
+            if (atr.ParameterType.Name.ToLower() == ("object[]") || atr.ParameterType.Name.ToLower() == ("object[]&"))
+                return ($"        dynamic[] {atr.Name} = null;");
+
+            if (atr.ParameterType.Name.ToLower().EndsWith("[]") || atr.ParameterType.Name.ToLower().EndsWith("[]&"))
+                return ($"        {atr.ParameterType.Name.Replace("&","")} {atr.Name} = null;");
+
+            switch (atr.ParameterType.Name.ToLower())
+            {
+                case "string":
+                case "string&":
+                    return ($"        string {atr.Name} = this.TestContext.DataRow[\"{atr.Name}\"].ToString();");
+                case "short":
+                case "short&":
+                    return ($"        short {atr.Name} = short.Parse(this.TestContext.DataRow[\"{atr.Name}\"].ToString());");
+                case "int":
+                case "int&":
+                    return ($"        int {atr.Name} = int.Parse(this.TestContext.DataRow[\"{atr.Name}\"].ToString());");
+                case "float":
+                case "float&":
+                    return ($"        float {atr.Name} = float.Parse(this.TestContext.DataRow[\"{atr.Name}\"].ToString());");
+                case "double":
+                case "double&":
+                    return ($"        double {atr.Name} = double.Parse(this.TestContext.DataRow[\"{atr.Name}\"].ToString());");
+                case "long":
+                case "long&":
+                    return ($"        long {atr.Name} = long.Parse(this.TestContext.DataRow[\"{atr.Name}\"].ToString());");
+                case "boolean":
+                case "boolean&":
+                    return ($"        bool {atr.Name} = bool.Parse(this.TestContext.DataRow[\"{atr.Name}\"].ToString());");
+                case "bool":
+                case "bool&":
+                    return ($"        bool {atr.Name} = bool.Parse(this.TestContext.DataRow[\"{atr.Name}\"].ToString());");
+                case "object":
+                case "object&":
+                    return ($"        dynamic {atr.Name} = null;");
+                case "datetime":
+                case "datetime&":
+                    return ($"        DateTime {atr.Name} = DateTime.Parse(this.TestContext.DataRow[\"{atr.Name}\"].ToString());");
+
+                default:
+                    return ($"        {atr.ParameterType.Name.Replace("&","")} {atr.Name} = new { atr.ParameterType.Name.Replace("&", "")}();");
+            }
+        }
 
 
         private void btGetFilePath_Click(object sender, EventArgs e)
