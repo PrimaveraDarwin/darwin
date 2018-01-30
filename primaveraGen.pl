c5ccc45e1555efc3d64cb0ac9d361fdeb06864ec
Julio-campos-primaverabss <35965534+Julio-campos-primaverabss@users.noreply.github.com>
2018-01-30 18:14:02 +0000

    update
    perl

--- primaveraGen.pl
+++ primaveraGen.pl
@@ -2,13 +2,22 @@
 use strict;
 use warnings;
 
-my @scripts = ("ServicesGenerator.sh","EditorGenerator.sh");
+
+my @scripts = ("ServicesGenerator","EditorsGenerator.sh");
 
 my @data_array = ("Base", "Sales","Purchases","EquipmentsFixedAssets","Accounting","Internal","HumanResources","CashManagement","ContactsOpportunities");
 
+my $string  = "";
 
 foreach my $script (@scripts) {
 	foreach my $data (@data_array) { 
-    		system("bash $script $data");
+    		$string = $string . "," . $data;
+		system("bash $script $data");
 	} 
 }
+
+substr($string, 0, 1) = "";
+
+#print "$string";
+system ("bash ExtensibilityEventsGenerator.sh $string");
+
